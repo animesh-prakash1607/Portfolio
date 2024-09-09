885cbe06c41fa639201c508fcdf6cbf7abcd4003
Animesh-Prakash <animeshp1607@gmail.com>
2024-09-09 11:41:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('#element2', {
+       strings:[
+        "<b>Web Developer.</b>",
+          "<b>Programmer.</b>",
+       ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1000,
+        startDelay: 400,
+        loop: true,
+      });
+});
