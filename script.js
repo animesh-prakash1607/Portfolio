60e928b1e84ec6306a7fb7af43861e26455119c2
Animesh-Prakash <animeshp1607@gmail.com>
2024-09-14 10:47:51 +0530

    Make changes

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
         "<b>Web Developer.</b>",
           "<b>Programmer.</b>",
        ],
-        typeSpeed: 50,
-        backSpeed: 30,
-        backDelay: 1000,
+        typeSpeed: 80,
+        backSpeed: 100,
+        backDelay: 1300,
         startDelay: 400,
         loop: true,
       });
-      document.querySelector(".elements li a").style.color="black";
+    document.querySelector(".elements li a").style.color="#00ff97"
 });
 function nav(){
   let nav_lists=document.querySelectorAll(".elements li");
@@ -21,45 +21,41 @@ function nav(){
     //   })
     // })
     nav_lists[0].addEventListener("click",()=>{
-      document.querySelector(".elements span").style.left="45px"
       let list=document.querySelectorAll(".elements li a");
      list.forEach((li)=>{
       li.style.color="white"
      })
-      list[0].style.color="black";
+      list[0].style.color="#00ff97";
       
     })
     nav_lists[1].addEventListener("click",()=>{
-      document.querySelector(".elements span").style.left="31vh"   
        let list=document.querySelectorAll(".elements li a");
        list.forEach((li)=>{
         li.style.color="white"
        })
-     list[1].style.color="black"
+        list[1].style.color="#00ff97";
+    
     })
     nav_lists[2].addEventListener("click",()=>{
-      document.querySelector(".elements span").style.left="54vh"
        let list=document.querySelectorAll(".elements li a");
        list.forEach((li)=>{
         li.style.color="white"
        })
-     list[2].style.color="black"
+     list[2].style.color="#00ff97";
     })
     nav_lists[3].addEventListener("click",()=>{
-      document.querySelector(".elements span").style.left="75vh"
        let list=document.querySelectorAll(".elements li a");
        list.forEach((li)=>{
         li.style.color="white"
        })
-     list[3].style.color="black"
+     list[3].style.color="#00ff97";
     })
     nav_lists[4].addEventListener("click",()=>{
-      document.querySelector(".elements span").style.left="98vh"
        let list=document.querySelectorAll(".elements li a");
        list.forEach((li)=>{
         li.style.color="white"
        })
-     list[4].style.color="black"
+     list[4].style.color="#00ff97";
     })
      
     let scroll=document.querySelectorAll(".elements li a")
@@ -75,11 +71,38 @@ function nav(){
 
    
 }
-function hamburger(){
-  let menu=document.querySelector(".ham");
-  menu.addEventListener("click",()=>{
-    document.querySelector(".left").style.left="0px";
+function hamburger() {
+  let menu = document.querySelector(".ham");
+  menu.addEventListener("click", () => {
+    document.querySelector(".left").style.display="block";
+    document.querySelector(".left").style.left = "0px";  // Show the menu when hamburger icon is clicked
+    document.querySelector(".nav").style.display = "none";
+  });
+
+  let close = document.querySelector("#close");
+  close.addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-100%";  // Hide the menu when close icon is clicked
+    document.querySelector(".nav").style.display = "flex";
+  });
+
+  // Add event listeners to the list items to hide the sidebar on click
+  let scroll2 = document.querySelectorAll(".elements2 li a");
+  scroll2.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      let pages = document.querySelector(this.getAttribute('href'));
+      pages.scrollIntoView({
+        behavior: 'smooth'  // Scroll to the corresponding section
+      });
+    });
+  });
+  scroll2.forEach(link=>{
+    link.addEventListener('click',function(event){
+      document.querySelector(".nav").style.display = "flex";
+      document.querySelector(".left").style.left = "-100%";
+    })
   })
 }
+
 nav();
 hamburger();
